dcbeb2bfdad152b61a5cbad771ddea9be9df36b7
sebastian nieto <juan.nieto-mo@mail.escuelaing.edu.co>
2021-04-14 16:00:02 -0500

    parte1

---
+++ src/main/java/eci/ieti/controller/RESTController.java
@@ -0,0 +1,75 @@
+package eci.ieti.controller;
+
+
+import eci.ieti.data.TodoRepository;
+import eci.ieti.data.model.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import com.mongodb.client.gridfs.model.GridFSFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RequestMapping("api")
+@RestController
+public class RESTController {
+
+
+   //TODO inject components (TodoRepository and GridFsTemplate)
+    @Autowired
+    GridFsTemplate gridFsTemplate;
+
+    @Autowired
+    TodoRepository todoRepository;
+
+    @RequestMapping("/files/{filename}")
+    public ResponseEntity<InputStreamResource> getFileByName(@PathVariable String filename) throws IOException {
+
+        //TODO implement method
+        GridFSFile file = gridFsTemplate.findOne(new Query().addCriteria(Criteria.where("filename").is(filename)));
+        if (file==null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        GridFsResource resource = gridFsTemplate.getResource(file.getFilename());
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(resource.getContentType()))
+                .body(new InputStreamResource(resource.getInputStream()));
+    }
+
+    @CrossOrigin("*")
+    @PostMapping("/files")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws IOException {
+
+        //TODO implement method
+        //Stores the file into MongoDB
+        gridFsTemplate.store(file.getInputStream(), file.getOriginalFilename(), file.getContentType());
+        return "http://localhost:8080/api/files/"+file.getOriginalFilename();
+    }
+
+    @CrossOrigin("*")
+    @PostMapping("/todo")
+    public Todo createTodo(@RequestBody Todo todo) {
+        //TODO implement method
+        return null;
+    }
+
+    @CrossOrigin("*")
+    @GetMapping("/todo")
+    public List<Todo> getTodoList() {
+        //TODO implement method
+        return null;
+    }
+
+}
